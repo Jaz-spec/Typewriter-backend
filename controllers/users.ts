fb22464c2ff8d08cfd4bdba165d52c65598240bd
Jaz-spec <jaz.edna@proton.me>
2025-05-06 11:52:35 +0100

    unfinished typecheck

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,4 +1,20 @@
-export default function sendData(req: any, res: any): any {
-	let message = { text: "it works!" };
-	res.json(message);
+import { fetchUsers } from "../lib/fetch-data";
+import { Request, Response } from "express";
+
+interface Data {
+	id: number;
+	created_at: string;
+	name: string;
+	number_of_logins: number | null;
+}
+
+
+export default async function sendData(req: Request, res: Response) {
+	let message: Data[] = await fetchUsers();
+	if (message) {
+
+		res.json(message);
+	} else {
+		return [];
+	} 
 }
--- lib/fetch-data.ts
+++ lib/fetch-data.ts
@@ -1,14 +1,15 @@
 import { supabase } from "../config/supabase-client";
-async function fetchUsers() {
+export async function fetchUsers(): Promise<any[]> {
 	let { data, error } = await supabase.from("Users").select("*");
 	if (error) {
 		console.error("Error fetching Users:", error);
-		throw error;
+		throw error;	
 	}
 
 	if (data) {
 		console.log("Data:", data);
+		return data;
+	} else {
+		return [];
 	}
-	return data;
 }
-export { fetchUsers };
